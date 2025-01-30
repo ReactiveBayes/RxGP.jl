af2f90d311ef587832f2a9e167a3f331b0840ccc
Documenter.jl <documenter@juliadocs.github.io>
2025-01-30 14:24:58 +0000

    build based on 6b61169

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RxGP","category":"page"},{"location":"#RxGP","page":"Home","title":"RxGP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RxGP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RxGP]","category":"page"}]
+}
