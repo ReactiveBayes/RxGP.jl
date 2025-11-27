b4f83818420fc07d2dfaf5c8aa9daaef3e44f135
Alex <119050107+ofSingularMind@users.noreply.github.com>
2025-11-27 11:12:41 +0100

    fix - syntax consistency between rules

--- src/rule/univariate_rules/theta.jl
+++ src/rule/univariate_rules/theta.jl
@@ -13,27 +13,27 @@
     mxuT_KuT = (θ) -> transpose(Ku_mxu(θ))
 
     if q_in isa Distribution
-        Ψ_0 = (θ) -> approximate_kernel_expectation(meta.method, (x) -> kernelmatrix(kernel(θ), [x], [x]), q_in)[1]
-        Ψ_1 = (θ) -> approximate_kernel_expectation(meta.method, (x) -> kernelmatrix(kernel(θ), [x], Xu), q_in)
-        Ψ_2 = (θ) -> approximate_kernel_expectation(meta.method, (x) -> kernelmatrix(kernel(θ), Xu, [x]) * kernelmatrix(kernel(θ), [x], Xu), q_in)
-        Ψ_3 = (θ) -> approximate_kernel_expectation(meta.method, (x) -> apply_mean_fn(x, mf) * kernelmatrix(kernel(θ), [x], Xu), q_in)
+        Ψ0 = (θ) -> approximate_kernel_expectation(meta.method, (x) -> kernelmatrix(kernel(θ), [x], [x]), q_in)[1]
+        Ψ1 = (θ) -> approximate_kernel_expectation(meta.method, (x) -> kernelmatrix(kernel(θ), [x], Xu), q_in)
+        Ψ2 = (θ) -> approximate_kernel_expectation(meta.method, (x) -> kernelmatrix(kernel(θ), Xu, [x]) * kernelmatrix(kernel(θ), [x], Xu), q_in)
+        Ψ3 = (θ) -> approximate_kernel_expectation(meta.method, (x) -> apply_mean_fn(x, mf) * kernelmatrix(kernel(θ), [x], Xu), q_in)
     else
         μ_in, Σ_in = mean_cov_vector_matrix(q_in)
-        Ψ_0 = (θ) -> kernelmatrix(kernel(θ), [μ_in], [μ_in])[1]
-        Ψ_1 = (θ) -> kernelmatrix(kernel(θ), [μ_in], Xu) 
-        Ψ_2 = (θ) -> kernelmatrix(kernel(θ), Xu, [μ_in]) * kernelmatrix(kernel(θ), [μ_in], Xu)
-        Ψ_3 = (θ) -> apply_mean_fn(μ_in, mf) * kernelmatrix(kernel(θ), [μ_in], Xu) 
+        Ψ0 = (θ) -> kernelmatrix(kernel(θ), [μ_in], [μ_in])[1]
+        Ψ1 = (θ) -> kernelmatrix(kernel(θ), [μ_in], Xu) 
+        Ψ2 = (θ) -> kernelmatrix(kernel(θ), Xu, [μ_in]) * kernelmatrix(kernel(θ), [μ_in], Xu)
+        Ψ3 = (θ) -> apply_mean_fn(μ_in, mf) * kernelmatrix(kernel(θ), [μ_in], Xu) 
     end
 
-    Ψ_2_Ku_mxu = (θ) -> Ψ_2(θ) * Ku_mxu(θ)
+    Ψ2_Ku_mxu = (θ) -> Ψ2(θ) * Ku_mxu(θ)
 
-    I1_θ = (θ) -> Ψ_0(θ) - tr( KuuF(θ) \ Ψ_2(θ) )
+    I1_θ = (θ) -> Ψ0(θ) - tr( KuuF(θ) \ Ψ2(θ) )
     I5_θ = (θ) -> (
-        - 2 * μ_y * jdotavx(Ψ_1(θ), ( μ_v - Ku_mxu(θ) ))
-        + tr( Rv * Ψ_2(θ) ) 
-        + jdotavx(mxuT_KuT(θ), Ψ_2_Ku_mxu(θ))
-        + 2 * jdotavx(Ψ_3(θ), (μ_v - Ku_mxu(θ)))  
-        - 2 * jdotavx(transpose(μ_v), Ψ_2_Ku_mxu(θ))
+        - 2 * μ_y * jdotavx(Ψ1(θ), ( μ_v - Ku_mxu(θ) ))
+        + tr( Rv * Ψ2(θ) ) 
+        + jdotavx(mxuT_KuT(θ), Ψ2_Ku_mxu(θ))
+        + 2 * jdotavx(Ψ3(θ), (μ_v - Ku_mxu(θ)))  
+        - 2 * jdotavx(transpose(μ_v), Ψ2_Ku_mxu(θ))
     )
 
     log_backwardmess = (θ) -> -0.5 * w_bar * (I1_θ(θ) + I5_θ(θ))
